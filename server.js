3acd3cc97413c61855bdc12ad0553d1b5ff208d4
Bolter87 <workw6132@gmail.com>
2022-03-03 12:24:20 +0100

    osef

--- server.js
+++ server.js
@@ -1,12 +1,22 @@
 
-const jsonServer = require('json-server');
+const express = require("express");
+const path = require("path");
+const app = express();
+app.use(express.static(__dirname + "/dist/my-app"));
+app.get("/*", function (req, res) {
+  res.sendFile(
+    path.join(__dirname + "/dist/my-app/index.html")
+  );
+});
+app.listen(process.env.PORT || 8080);
+
+const jsonServer = require("json-server");
 const server = jsonServer.create();
-const router = jsonServer.router('db/db.json');
+const router = jsonServer.router("./db/data.json");
 const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 80;
 
 server.use(middlewares);
 server.use(router);
-
-server.listen(port);
-
+server.listen(3000, () => {
+  console.log("JSON Server is running");
+});
