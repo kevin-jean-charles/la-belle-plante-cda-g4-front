b07ce02597e2bb4efa39ce974da0b4068460eb95
Bolter87 <workw6132@gmail.com>
2022-03-03 11:06:30 +0100

    commit

---
+++ src/app/components/nav-bar/nav-bar.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { PlantService } from 'src/app/services/plant.service';
+
+@Component({
+  selector: 'app-nav-bar',
+  templateUrl: './nav-bar.component.html',
+  styleUrls: ['./nav-bar.component.scss']
+})
+export class NavBarComponent {
+  public title: string;
+  public lengthListProduct!: number;
+
+  constructor(private plantService: PlantService) {
+    this.title = '🪴 La Belle Plante';
+    
+    this.plantService.subjectListProduct$.subscribe(data => {
+      this.lengthListProduct = data.length;
+    })
+  }
+
+}
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  baseUrlApi: 'https://dev-environment-formation.herokuapp.com:3000'
 };
