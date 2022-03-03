c53b60afe0df2a15293bae3c2f937189ac437502
Bolter87 <workw6132@gmail.com>
2022-03-03 12:54:47 +0100

    osefff

--- server.js
+++ server.js
@@ -2,21 +2,10 @@
 const express = require("express");
 const path = require("path");
 const app = express();
-app.use(express.static(__dirname + "/dist/my-app"));
+app.use(express.static(__dirname + "/dist/la-belle-plante"));
 app.get("/*", function (req, res) {
   res.sendFile(
-    path.join(__dirname + "/dist/my-app/index.html")
+    path.join(__dirname + "/dist/la-belle-plante/index.html")
   );
 });
 app.listen(process.env.PORT || 8080);
-
-const jsonServer = require("json-server");
-const server = jsonServer.create();
-const router = jsonServer.router("./db/data.json");
-const middlewares = jsonServer.defaults();
-
-server.use(middlewares);
-server.use(router);
-server.listen(3000, () => {
-  console.log("JSON Server is running");
-});
