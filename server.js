cc741e9dc8cdbc9ef7322712d4c055ec56ea7276
Bolter87 <workw6132@gmail.com>
2022-03-03 11:13:22 +0100

    osef

--- server.js
+++ server.js
@@ -3,11 +3,11 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist/la-belle-plante'));
+app.use(express.static(__dirname + '/dist/my-app'));
 app.get('/*', function (req, res) {
-    res.sendFile(path.join(__dirname + '/dist/la-belle-plante/index.html'));
+    res.sendFile(path.join(__dirname + '/dist/my-app/index.html'));
 });
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 80);
 
 
 const jsonServer = require('json-server')
